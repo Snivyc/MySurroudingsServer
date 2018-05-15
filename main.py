d521b8566331f001cd8e9afe28393172f519c729
Snivy <snivy23@outlook.com>
2018-05-16 00:16:45 +0800

    5.16 new

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sqlite3
+import functools
 from math import radians, fabs, cos, sin, sqrt, asin
 
 from flask import Flask, send_file, jsonify, Response, url_for, request, session, g
@@ -36,7 +37,31 @@ def close_db(e=None):
     if db is not None:
         db.close()
 
-app.teardown_appcontext(close_db)
+
+# @app.before_request
+# def load_logged_in_user():
+#     user_id = session.get('user_id')
+#
+#     if user_id is None:
+#         g.user = None
+#     else:
+#         g.user = get_db().execute(
+#             'SELECT * FROM user WHERE id = ?', (user_id,)
+#         ).fetchone()
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        user_id = int(request.form['ID'])
+        user = get_db().execute(
+            'SELECT * FROM user WHERE AccountID = ?', (user_id,)
+        ).fetchone()
+        if user is None:
+            return "false"
+        return view(**kwargs)
+
+    return wrapped_view
 # @app.route('/test/')
 # def test():
 #     print('fuck')
@@ -47,11 +72,14 @@ app.teardown_appcontext(close_db)
 @app.route('/', methods=['GET', 'POST'])
 def main():
     if request.method == 'POST':
+    # if True:
         x = float(request.form['x'])
         y = float(request.form['y'])
+        # x=32.05
+        # y=118.75
         dx = 2/111.7
         dy = 2/(111.7 * cos(radians(x)))
-        print(dx, dy, cos(radians(x)))
+        # print(dx, dy, cos(radians(x)))
         db = get_db()
         temp = db.execute(
             'SELECT * FROM POINT WHERE X < ? AND X > ? AND Y < ? AND Y > ?',(x+dx, x-dx, y+dy, y-dy)
@@ -64,27 +92,28 @@ def main():
         for i in temp:
             distance = cal_distance(i["x"], i["y"])
             if distance < 2:
-                lst.append({"x":i["x"], "y":i["y"], "information": i["information"], "distance": distance})
+                lst.append({"x":i["x"], "y":i["y"], "information": i["information"], "distance": int(distance*1000)})
         lst.sort(key=lambda t : t["distance"])
-        print(lst[0:10])
+        # print(lst[0:10])
     # return Response('test.json',mimetype='application/json')
     # print(url_for('static', filename='test.json',))
     # return url_for('static', filename='test.json',)
 
         return jsonify(lst)
-    return "SB"
+    return send_file('static/test.json',mimetype='application/json')
 
 @app.route('/search/<keyword>', methods=['GET', 'POST'])
+@login_required
 def search(keyword):
     if request.method == 'POST':
         x = float(request.form['x'])
         y = float(request.form['y'])
         dx = 2/111.7
         dy = 2/(111.7 * cos(radians(x)))
-        print(dx, dy, cos(radians(x)))
+        # print(dx, dy, cos(radians(x)))
         db = get_db()
         temp = db.execute(
-            'SELECT * FROM POINT WHERE Information LIKE ? AND X < ? AND X > ? AND Y < ? AND Y > ?',("%"+keyword+"%",x+dx, x-dx, y+dy, y-dy)
+            'SELECT * FROM POINT WHERE Information LIKE ? AND X < ? AND X > ? AND Y < ? AND Y > ?' , ("%"+keyword+"%",x+dx, x-dx, y+dy, y-dy)
         ).fetchall()
         lst = []
         def cal_distance(_x, _y):
@@ -94,9 +123,9 @@ def search(keyword):
         for i in temp:
             distance = cal_distance(i["x"], i["y"])
             if distance < 2:
-                lst.append({"x":i["x"], "y":i["y"], "information": i["information"], "distance": distance})
+                lst.append({"x":i["x"], "y":i["y"], "information": i["information"], "distance": int(distance*1000)})
         lst.sort(key=lambda t : t["distance"])
-        print(lst[0:10])
+        # print(lst[0:10])
     # return Response('test.json',mimetype='application/json')
     # print(url_for('static', filename='test.json',))
     # return url_for('static', filename='test.json',)
@@ -123,7 +152,7 @@ def login():
 
         if error is None:
 
-            return jsonify({'isSuccess':True})
+            return jsonify({'isSuccess':True, 'ID':user["AccountID"]})
 
 
     return jsonify({'isSuccess':False})
@@ -157,6 +186,44 @@ def signup():
             db.execute(
                 'INSERT INTO user (account, password) VALUES (?, ?)',
                 (account, password)
+            )
+            db.commit()
+            return jsonify({'isSuccess': True, 'errorInformation': ""})
+        return jsonify({'isSuccess': False, 'errorInformation': error})
+
+    return jsonify({'isSuccess':False, 'errorInformation':"不接受get"})
+
+
+@app.route('/submitinfo', methods=['GET', 'POST'])
+@login_required
+def submitinfo():
+
+    if request.method == 'POST':
+
+        ID = request.form['ID']
+        # password = request.form['password']
+        info = request.form['info']
+        x = float(request.form['x'])
+        y = float(request.form['y'])
+        db = get_db()
+        error = None
+
+        if not info:
+            error = 'info is required'
+
+        # if not account:
+        #     error = 'Account is required.'
+        # elif not password:
+        #     error = 'Password is required.'
+        # elif db.execute(
+        #         'SELECT AccountID FROM user WHERE Account = ?', (account,)
+        # ).fetchone() is not None:
+        #     error = 'User {} is already registered.'.format(account)
+
+        if error is None:
+            db.execute(
+                'INSERT INTO Point (author_id, X, Y, Information) VALUES (?, ?, ?, ?)',
+                (ID, x, y, info)
             )
             db.commit()
             return jsonify({'isSuccess': True, 'errorInformation': ""})
